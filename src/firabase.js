8f37b496ab3f39a69de106ffa9e1c8f318cb5cea
arthurloni <166772542+arthurloni@users.noreply.github.com>
2025-02-16 16:41:09 -0400

    Update firabase.js

--- src/firabase.js
+++ src/firabase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCR75Qtpdk1O_tP673RH3Yc718l9i4wiCI",
     authDomain: "boteco-loni.firebaseapp.com",
     projectId: "boteco-loni",
-    storageBucket: "boteco-loni.appspot.com", // Correção aqui
+    storageBucket: "boteco-loni.appspot.com",
     messagingSenderId: "1008295567570",
     appId: "1:1008295567570:web:f399eb3913a1832cf18d0b",
     measurementId: "G-0671TCF9NY"
@@ -14,4 +14,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, collection, addDoc, getDocs };
+console.log("🔥 Firestore conectado:", db);
+
+export { db };
